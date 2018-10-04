4aac9bbf12588156c09646d097cdb37d98b35ad5
shiwendr <shivendrachaudhary05@gmail.com>
2018-10-04 15:03:38 -0500

    latest build

---
+++ assets/assets/js/cropper.js
@@ -0,0 +1,88 @@
+
+function readURL(input) {
+
+    var fuData = document.getElementById('reg-upload-pic');
+    var FileUploadPath = fuData.value;
+
+    //To check if user upload any file
+    if (FileUploadPath == '') {
+        alert("Please upload an image");
+        return;
+
+    } else {
+        var Extension = FileUploadPath.substring(
+            FileUploadPath.lastIndexOf('.') + 1).toLowerCase();
+
+        //The file uploaded is an image
+
+        if (Extension == "gif" || Extension == "png" || Extension == "bmp"
+            || Extension == "jpeg" || Extension == "jpg") {
+
+        }
+
+        //The file upload is NOT an image
+        else {
+            alert("Photo only allows file types of GIF, PNG, JPG, JPEG and BMP. ");
+            return;
+        }
+    }
+
+    $('#user_image').cropper('destroy')
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#user_image').attr('src', e.target.result)
+        };
+        reader.readAsDataURL(input.files[0]);
+        setTimeout(initCropper, 500);
+    }
+}
+
+function initCropper() {
+
+    var $image = $('#user_image');
+    $image.cropper({
+        aspectRatio: 1 / 1,
+        crop: function (event) {
+        }
+    });
+    // Get the Cropper.js instance after initialized
+    var cropper = $image.data('cropper');
+    // On crop button clicked
+    document.getElementById('crop_button').addEventListener('click', function () {
+        $('#myModal').modal('hide');
+        /**
+         * Save Croped Image to Server
+         */
+        var img_base64 = cropper.getCroppedCanvas().toDataURL("image/jpeg", 0.9);
+        // var formData = new FormData();
+        // formData.append('img_base64', img_base64);
+        var str = img_base64;
+        str = str.substring(str.indexOf("base64,") + 7);
+        $("#uploaded_img").attr('src', img_base64);
+        document.getElementById('profileimage').value = str;
+        console.log(document.getElementById('profileimage').value);
+        // $.ajax('api/users/create-profile-image', {
+        //     method: "POST",
+        //     data: formData,
+        //     processData: false,
+        //     contentType: 'application/x-www-form-urlencoded',
+        //     success: function (respdata) {
+        //         if (respdata.code == 200) {
+        //             $('#uploaded_img').attr('src', respdata.url);
+        //             $('#profile_img_url').val(respdata.url);
+        //         }
+        //     },
+        //     error: function () {
+        //         console.log('Upload error');
+        //     }
+        // });
+    })
+}
+
+$(document).ready(function () {
+    $("#cropImage").on("hidden.bs.modal", function () {
+        $('#user_image').cropper('destroy');
+        $('#user_image').attr('src', '/public/images/user.png');
+    });
+});
